a6c7463d16075fce1409aa4d9aa948b30f3e76c8
MisterNSA <53508377+MisterNSA@users.noreply.github.com>
2021-02-25 15:35:04 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// Dieser Code ist dreist aus dem Internet kopiert. Ich habe mich erst im Nachhinein in die Thematik eingelesen.
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
